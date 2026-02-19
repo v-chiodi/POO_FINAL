c21884946f7f1e08547e1e4dfa59ac7c1695c8e0
V-chiodi <chiodi290@gmail.com>
2026-02-19 12:15:23 -0300

    nuevo parcial

--- src/Parcial2024/ColeccionPriv.java
+++ src/Parcial2024/ColeccionPriv.java
@@ -1,4 +1,15 @@
 package Parcial2024;
 
+import Parcial2024.condiciones.Condicion;
+
 public class ColeccionPriv extends Coleccion {
+
+    public ColeccionPriv(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public ElementoFig getCopia(Condicion c) {
+        return null;
+    }
 }
--- src/Parcial2024/FiguDorada.java
+++ src/Parcial2024/FiguDorada.java
@@ -1,5 +1,7 @@
 package Parcial2024;
 
+import Parcial2024.condiciones.Condicion;
+
 import java.util.ArrayList;
 
 public class FiguDorada extends Figurita {
@@ -7,8 +9,28 @@ public class FiguDorada extends Figurita {
     private int nroUnidades;
     private int otroValor;
 
+    public FiguDorada(String nombre, String marca, String album, int numFig, int precio, Condicion c, int nroUnidades, int otroValor) {
+        super(nombre, marca, album, numFig, precio);
+        this.c = c;
+        this.nroUnidades = nroUnidades;
+        this.otroValor = otroValor;
+    }
 
-    public ArrayList<ElementoFig> getCopia(){
+    @Override
+    public int getPrecio() {
+        if(c.cumple(this)) {
+            return super.getPrecio();
+        }
+        return otroValor;
+
+    }
+
+    @Override
+    public ElementoFig getCopia(Condicion c){
+        if (c.cumple(this)){
+            return new FiguDorada(getNombre(),getMarca(),getAlbum(),getNumFig(),getPrecio(),c,nroUnidades,otroValor);
+        }
+        return null;
 
     }
 }
